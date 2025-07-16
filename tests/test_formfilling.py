e54e49084bf524b134d902d45b129e7a022bdb6a
Daniyal Fahim <daniyal236fahim@example.com>
2025-07-16 02:33:10 -0700

    Updating my Automation file to complete the process and go through the whole website : )

--- tests/test_formfilling.py
+++ tests/test_formfilling.py
@@ -8,17 +8,30 @@ def test_formfilling(page):
     page.wait_for_timeout(2000)  # Delay to preview login
 
     page.click('#add-to-cart-sauce-labs-fleece-jacket')
+    page.click('#add-to-cart-sauce-labs-backpack')
+    page.click('#add-to-cart-sauce-labs-onesie')
+    page.click('#add-to-cart-sauce-labs-bolt-t-shirt')
+
     page.wait_for_timeout(2000)  # Delay to preview login
     page.click('.shopping_cart_link')  # Fixed selector with underscore
     page.wait_for_timeout(2000)  # Delay to preview login
 
     page.click('#checkout')  # Fixed selector with underscore
     page.wait_for_timeout(2000)  # Delay to preview checkout
-    page.fill('#first-name', 'John')  # Using id="first-name"
-    page.fill('#last-name', 'Doe')    # Using id="last-name"
-    page.fill('#postal-code', '12345')  # Using id="postal-code"
-    page.wait_for_timeout(1000)  # Delay to preview form filling
+    page.fill('#first-name', 'Dan')  # Using id="first-name"
+    page.fill('#last-name', 'Ven')    # Using id="last-name"
+    page.fill('#postal-code', '564321')  # Using id="postal-code"
+    page.wait_for_timeout(2000)  # Delay to preview form filling
     page.click('#continue')  # Using id="continue"
+
+    page.wait_for_timeout(2000)  # Delay to preview order review
+    assert page.url == "https://www.saucedemo.com/checkout-step-two.html"
+    page.screenshot(path="screenshots/order_review.png")
+    page.click('#finish')  # Using id="finish"
+    page.wait_for_timeout(2000)  # Delay to preview form filling
+    page.click('#back-to-products')
+    page.wait_for_timeout(2000)  # Delay to preview back to products
+
 
     page.click('#react-burger-menu-btn')  # Open menu
     page.wait_for_selector('#logout_sidebar_link')  # Wait for logout link to be visible
